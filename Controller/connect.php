d9ea746912213de9f8adf47e682da2d1f30709b1
Nemolite <g25092011@mail.ru>
2017-10-13 18:21:51 +0300

    start

---
+++ Controller/connect.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 27.09.2017
+ * Time: 10:14
+ */
+namespace Controller;
+
+use PDO;
+
+class connect {
+
+    private  $dbconnect;
+
+    public function __construct()
+    {
+        $this->setConnect();
+    }
+
+    /**
+     * @return $this
+     */
+
+
+     public function setConnect()
+     {
+         $ini_array = parse_ini_file("config/config.ini");
+
+         $dsn = 'mysql:dbname='.$ini_array[dbname].';host='.$ini_array[host];
+
+         $user = $ini_array[username];
+         $password = $ini_array[userpass];
+
+         $this->dbconnect  = new PDO($dsn, $user, $password,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+         return $this;
+
+     }
+
+
+    /**
+     * @return array
+     */
+    public function query($sql,$value=[]){
+
+        $line = $this->dbconnect->prepare($sql);
+
+        $line->execute($value);
+
+        $result = $line->fetchAll(PDO::FETCH_ASSOC);
+
+        if($result=== false){
+            return [];
+        }
+
+        return $result;
+
+    }
+
+    public function execute($sql, $values = [])
+    {
+        $sth = $this->dbconnect->prepare($sql);
+        return $sth->execute($values);
+    }
+
+}
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 13.10.2017
+ * Time: 16:44
+ */
+
+define('ROOT_DIR',__DIR__);
+
+require_once 'config/bootstrap.php';
