eef754ea1c2b2cda3993e8e2d3cd19587cee6e29
Nemolite <g25092011@mail.ru>
2017-10-20 11:42:17 +0300

    game script

--- View/assets/game.js
+++ View/assets/game.js
@@ -1,7 +1,7 @@
 /**
  * Created by user on 18.10.2017.
  */
-
+// The block is AJAX for send word to server
 function createObject() {
     var request_type;
     var browser = navigator.appName;
@@ -16,11 +16,33 @@ function createObject() {
 
 var xhr = createObject();
 
+function searchResults() {
+    if(xhr.readyState == 4&& this.status == 200){
+        var response = xhr.responseText;
+        document.getElementById('debug').innerHTML = response;
+    }
+}
+
+// end AJAX
+
+
+
+
+// action
+
 function transfer() {
     var word = document.forms.wordform.word.value;
     console.log(word); //debug
 
     //validation
+    valid(word);
+
+    check_to_repeat(word);
+}
+
+
+
+function valid(word) {
 
     if (3>=word.length){
         document.getElementById('debug').innerHTML = "Ваше слово слишком короткое";
@@ -28,20 +50,77 @@ function transfer() {
 
     // проверка на гласные а, о, и, е, ё, э, ы, у, ю, я
 
-    var pattern =/^[аоиеёэыуюя]$/
-   // if ()
-/*
-    var parametr =encodeURI(word);
-    xhr.open('get', '/game?var='+parametr);
-    xhr.onreadystatechange =  searchResults;
-    xhr.send(null);
-    */
+    var pattern_g =/^[аоиеёэыуюя]$/
+
+    // проверка на согласные б, в, г, д, ж, з, й, к, л, м, н, п, р, с, т, ф, х, ц, ч, ш, щ
+
+    var pattern_s =/^[бвгджзйклмнпрстфхцчшщ]$/
+
+    //  больше трех подряд гласных или согласных букв
+
+        // code
+
+    // не содержит какие-либо символы, кроме символов русского алфавита
+
+    // code
+
+    var check = check_to_repeat(word);
+
+    if (check) {
+        // слово в соответствующее поле
+    }
+
 
 }
 
-function searchResults() {
-    if(xhr.readyState == 4&& this.status == 200){
-        var response = xhr.responseText;
-        document.getElementById('debug').innerHTML = response;
+function check_to_repeat(word){ // проверяем и запоминаем
+
+    // filter
+
+    myArray = stackWords();
+
+    myArray = myArray.filter(function(x) {
+        return x !== undefined && x !== null;
+    });
+
+    console.log(myArray);
+
+    var parametr = myArray.indexOf(word);
+
+    if (-1==parametr){
+        stackWords(word);
+
+        return true;
+    }else{
+
+        if (isNumber(parametr)) {
+            document.getElementById('debug').innerHTML = "Уже было";
+            return false;
+        }
     }
+
+
+
+    //closure
+
+    function stackWords() {
+        var numberOfCalls = [];
+        if (arguments.length!=0)
+            var w = arguments[0];
+        return function(w) {
+            numberOfCalls.push(w);
+            console.log(numberOfCalls);
+            return numberOfCalls;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 }
